753de9df8caa411c0f11ca16cd121056c1877d7d
Ababei Andrei <AbabeiAndrei@outlook.com>
2018-07-31 21:54:59 +0300

    First Commit

---
+++ CinemaScheduler/Controllers/CinemaController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaScheduler.Controllers
+{
+    public class CinemaController : Controller
+    {
+        public IActionResult Index() => View(); 
+
+        public IActionResult Login() => View();
+    }
+}
